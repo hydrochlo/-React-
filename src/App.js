2e33295bfbbb666bda784eaf283dd40e067479b7
Shahadat Hossain <sht.kcn@gmail.com>
2026-06-02 02:55:57 +0600

    componenets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Card from './components/Card.js';
+import Data from './data.json';
 
 function App(){
+
+    let items = [];
+    for(let i=0; i<Data.length; i++){
+      items.push(<Card title={Data[i].title} desc={Data[i].desc} />);
+    }
     return <>
         <h1 className='headingStyle'>Hello World!!! </h1>
+
         <Card title = "Solve 2 problems from CF" desc="solve this without any expternal help." />
         <Card title = "Push come code to GITHUB" desc="write some everyday and push them to GITHUB." />
+        {items}
     </>
 }
 
